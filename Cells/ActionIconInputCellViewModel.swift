2efbcc51109bfc7a24d0869423598e3823a94f46
Viorel Mihalache <viorel.mihalache@gmail.com>
2017-06-27 10:07:34 +0100

    refactored files to enable compilation

---
+++ Cells/ActionIconInputCellViewModel.swift
@@ -0,0 +1,38 @@
+import ReactiveSwift
+import Result
+
+public struct ActionIconInputCellViewModel: Selectable {
+    private let visualDependencies: VisualDependenciesProtocol
+    let title: String
+    let input: Property<String>
+    let inputTextAlignment: TextAlignment
+    let selectionStyle: UITableViewCellSelectionStyle = .gray
+    let isSelected: Action<Void, Void, NoError>
+    let accessory: UITableViewCellAccessoryType
+    let icon: UIImage
+    
+    public init(visualDependencies: VisualDependenciesProtocol,
+         icon: UIImage,
+         title: String,
+         input: Property<String>,
+         inputTextAlignment: TextAlignment = .right,
+         selected: Action<Void, Void, NoError>,
+         accessory: UITableViewCellAccessoryType = .disclosureIndicator) {
+        self.visualDependencies = visualDependencies
+        self.icon = icon
+        self.title = title
+        self.inputTextAlignment = inputTextAlignment
+        self.input = input
+        self.isSelected = selected
+        self.accessory = accessory
+        
+    }
+
+    func applyTitleStyle(to label: UILabel) {
+        visualDependencies.styles.labelFormFieldTitle.apply(to: label)
+    }
+
+    func applyInputStyle(to label: UILabel) {
+        visualDependencies.styles.labelFormFieldInputValue(alignment: inputTextAlignment).apply(to: label)
+    }
+}
