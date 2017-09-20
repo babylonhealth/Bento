a195b22b330f6fee287e27a983c73c50697c538f
Catarina Figueiredo <anacatarina.figueiredo@babylonhealth.com>
2017-09-20 14:53:10 +0100

    Remove unused files.

--- TitledDetailListCell/DetailItemView.swift
+++
@@ -1,9 +0,0 @@
-class DetailItemView: UIView {
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var descriptionLabel: UILabel!
-
-    func setup(viewModel: DetailItemViewModel) {
-        viewModel.applyTitleStyle(to: titleLabel)
-        viewModel.applyDescriptionStyle(to: descriptionLabel)
-    }
-}
