63f99c765d5209f1905475c7387832fc95fdd35c
sergdort <sergdort@gmail.com>
2018-01-17 16:27:23 +0000

    Add example app

---
+++ Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Sergey Shulga on 17/01/2018.
+//  Copyright © 2018 babylonhealth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
