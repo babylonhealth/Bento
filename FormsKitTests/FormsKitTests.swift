612a86185a91c70f3dd42d4eb3003291263b58d9
sergdort <sergdort@gmail.com>
2018-02-02 14:28:19 +0000

    Setup CI

---
+++ FormsKitTests/FormsKitTests.swift
@@ -0,0 +1,13 @@
+//
+//  FormsKitTests.swift
+//  FormsKitTests
+//
+//  Created by Sergey Shulga on 02/02/2018.
+//  Copyright © 2018 babylonhealth. All rights reserved.
+//
+
+import XCTest
+
+class FormsKitTests: XCTestCase {
+    func testExample() {}
+}
