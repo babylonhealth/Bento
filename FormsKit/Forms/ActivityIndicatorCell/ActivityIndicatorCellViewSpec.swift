3c38df69cd2b40592542fb31dcecdb248a1708d9
sergdort <sergdort@gmail.com>
2018-02-02 15:23:42 +0000

    Remove old files from the repo

--- FormsKit/Forms/ActivityIndicatorCell/ActivityIndicatorCellViewSpec.swift
+++
@@ -1,7 +0,0 @@
-public struct ActivityIndicatorCellViewSpec {
-    public let cellStyle: UIViewStyle<UIView>?
-
-    public init(cellStyle: UIViewStyle<UIView>?) {
-        self.cellStyle = cellStyle
-    }
-}
