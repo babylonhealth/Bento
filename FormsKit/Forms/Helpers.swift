aca7196bc9c0a378cab16ab1ecc70a6a30f3720d
sergdort <sergdort@gmail.com>
2018-02-06 10:26:07 +0000

    Use pointer instead of String

--- FormsKit/Forms/Helpers.swift
+++ FormsKit/Forms/Helpers.swift
@@ -28,12 +28,12 @@ extension UIView {
 
 extension UITableView {
     private struct AssociatedKey {
-        static var key = "FormsKit.UITableView.AssociatedKey.key"
+        static let key = UnsafeMutablePointer<CChar>.allocate(capacity: 1)
     }
     func getAdapter<SectionId, RowId>() -> SectionedFormAdapter<SectionId, RowId> {
-        guard let adapter = objc_getAssociatedObject(self, &AssociatedKey.key) as? SectionedFormAdapter<SectionId, RowId> else {
+        guard let adapter = objc_getAssociatedObject(self, AssociatedKey.key) as? SectionedFormAdapter<SectionId, RowId> else {
             let adapter = SectionedFormAdapter<SectionId, RowId>(with: self)
-            objc_setAssociatedObject(self, &AssociatedKey.key, adapter, .OBJC_ASSOCIATION_RETAIN)
+            objc_setAssociatedObject(self, AssociatedKey.key, adapter, .OBJC_ASSOCIATION_RETAIN)
             return getAdapter()
         }
         return adapter
