7e3062c25bd3edea47dce427408352a756049085
sergdort <sergdort@gmail.com>
2018-04-11 15:58:21 +0100

    Rename Bento to Box

--- Bento/Bento/Box.swift
+++ Bento/Bento/Box.swift
@@ -21,25 +21,25 @@ infix operator |--*: NodeConcatenationPrecedence
 infix operator |--?: NodeConcatenationPrecedence
 infix operator <>: ComposingPrecedence
 
-public struct Bento<SectionId: Hashable, RowId: Hashable> {
+public struct Box<SectionId: Hashable, RowId: Hashable> {
     public let sections: [Section<SectionId, RowId>]
 
     public init(sections: [Section<SectionId, RowId>]) {
         self.sections = sections
     }
 
-    public static var empty: Bento {
-        return Bento(sections: [])
+    public static var empty: Box {
+        return Box(sections: [])
     }
 }
 
-public func |-+<SectionId, RowId>(lhs: Bento<SectionId, RowId>, rhs: Section<SectionId, RowId>) -> Bento<SectionId, RowId> {
-    return Bento(sections: lhs.sections + [rhs])
+public func |-+<SectionId, RowId>(lhs: Box<SectionId, RowId>, rhs: Section<SectionId, RowId>) -> Box<SectionId, RowId> {
+    return Box(sections: lhs.sections + [rhs])
 }
 
 extension UITableView {
-    public func render<SectionId, RowId>(_ bento: Bento<SectionId, RowId>) {
+    public func render<SectionId, RowId>(_ box: Box<SectionId, RowId>) {
         let adapter: SectionedFormAdapter<SectionId, RowId> = getAdapter()
-        adapter.update(sections: bento.sections)
+        adapter.update(sections: box.sections)
     }
 }
