26067aac7d12f0f2acfd8f3be3fdf99d64b132ca
sergdort <sergdort@gmail.com>
2018-04-13 10:46:11 +0100

    Add animation options (#17)
    * Add animation options
    
    * Set default arguments for init

--- Bento/Bento/Box.swift
+++ Bento/Bento/Box.swift
@@ -40,6 +40,21 @@ public func |-+<SectionId, RowId>(lhs: Box<SectionId, RowId>, rhs: Section<Secti
 extension UITableView {
     public func render<SectionId, RowId>(_ box: Box<SectionId, RowId>) {
         let adapter: SectionedFormAdapter<SectionId, RowId> = getAdapter()
-        adapter.update(sections: box.sections)
+        adapter.update(sections: box.sections, with: TableViewAnimation())
+    }
+
+    public func render<SectionId, RowId>(_ box: Box<SectionId, RowId>, animated: Bool) {
+        let adapter: SectionedFormAdapter<SectionId, RowId> = getAdapter()
+        if animated {
+            adapter.update(sections: box.sections, with: TableViewAnimation())
+        } else {
+            adapter.update(sections: box.sections)
+        }
+    }
+
+    public func render<SectionId, RowId>(_ box: Box<SectionId, RowId>, with animation: TableViewAnimation) {
+        let adapter: SectionedFormAdapter<SectionId, RowId> = getAdapter()
+
+        adapter.update(sections: box.sections, with: animation)
     }
 }
