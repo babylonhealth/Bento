a230d0f158fea3a9f6ca8b10ff443d815443ee7e
Michael Brown <me@michael-brown.net>
2018-05-17 21:19:58 +0100

    Add |-? operator and allow |-+ and |---+ to work with optionals (#28)
    * Add |-? operator.
    Allow |-+ and |---+ to work with optionals.
    Tests for conditionals and optionals.
    
    * Removed |-+ and |---+ accepting optional in favour of
    |-? and |---? which accept any optional instead of a conditional.
    Expanded the |---? operators to work with Node + Node and [Node] + Node
    to match the |---+ operators.
    
    * Updated CI to Xcode 9.3
    
    * Upgrade to Circle CI 2.0.
    Minor formatting changes.
    
    * Fighting with YAML.

--- Bento/Bento/If.swift
+++ Bento/Bento/If.swift
@@ -6,8 +6,21 @@ public struct If<T> {
         return If(condition: condition, generator: generator)
     }
 
+    public static func iff(_ condition: @autoclosure @escaping () -> Bool, _ generator: @escaping () -> T) -> If<T> {
+        return If(condition: condition, generator: generator)
+    }
+
     public static func iff(_ condition: @escaping () -> Bool, _ generator: @escaping () -> T) -> If<T> {
         return If(condition: condition, generator: generator)
+    }
+}
+
+public struct Some<T, U> {
+    let optional: T?
+    let generator: (T) -> U
+
+    public static func some(_ optional: T?, _ generator: @escaping (T) -> U) -> Some<T, U> {
+        return Some(optional: optional, generator: generator)
     }
 }
 
@@ -25,4 +38,58 @@ public func |---?<SectionId, RowId>(lhs: Section<SectionId, RowId>, rhs: If<[Nod
             |---* rhs.generator()
     }
     return lhs
+}
+
+public func |---?<T, SectionId, RowId>(lhs: Section<SectionId, RowId>, rhs: Some<T, Node<RowId>>) -> Section<SectionId, RowId> {
+    return rhs.optional.map { value in
+        lhs |---+ rhs.generator(value)
+    } ?? lhs
+}
+
+public func |---?<T, SectionId, RowId>(lhs: Section<SectionId, RowId>, rhs: Some<T, [Node<RowId>]>) -> Section<SectionId, RowId> {
+    return rhs.optional.map { value in
+        lhs |---* rhs.generator(value)
+    } ?? lhs
+}
+
+public func |-?<SectionId, RowId>(lhs: Box<SectionId, RowId>, rhs: If<Section<SectionId, RowId>>) -> Box<SectionId, RowId> {
+    if rhs.condition() {
+        return lhs
+            |-+ rhs.generator()
+    }
+    return lhs
+}
+
+public func |-?<T, SectionId, RowId>(lhs: Box<SectionId, RowId>, rhs: Some<T, Section<SectionId, RowId>>) -> Box<SectionId, RowId> {
+    return rhs.optional.map { value in
+        lhs |-+ rhs.generator(value)
+    } ?? lhs
+}
+
+public func |---?<Identifier>(lhs: Node<Identifier>, rhs: If<Node<Identifier>>) -> [Node<Identifier>] {
+    if rhs.condition() {
+        return [lhs, rhs.generator()]
+    }
+
+    return [lhs]
+}
+
+public func |---?<Identifier>(lhs: [Node<Identifier>], rhs: If<Node<Identifier>>) -> [Node<Identifier>] {
+    if rhs.condition() {
+        return lhs + [rhs.generator()]
+    }
+
+    return lhs
+}
+
+public func |---?<T, Identifier>(lhs: Node<Identifier>, rhs: Some<T, Node<Identifier>>) -> [Node<Identifier>] {
+    return rhs.optional.map { value in
+        [lhs, rhs.generator(value)]
+        } ?? [lhs]
+}
+
+public func |---?<T, Identifier>(lhs: [Node<Identifier>], rhs: Some<T, Node<Identifier>>) -> [Node<Identifier>] {
+    return rhs.optional.map { value in
+        lhs + [rhs.generator(value)]
+        } ?? lhs
 }
