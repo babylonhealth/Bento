82f6752841f7355df21861de970f4fb98664cc62
sergdort <sergdort@gmail.com>
2018-08-01 11:42:03 +0100

    Set clear color as a background color for containers (#37)

--- Bento/Views/CollectionViewContainerCell.swift
+++ Bento/Views/CollectionViewContainerCell.swift
@@ -3,6 +3,15 @@ import UIKit
 final class CollectionViewContainerCell: UICollectionViewCell {
     var containedView: UIView? = nil
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .clear
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     func install(view: UIView) {
         containedView = view
         contentView.addSubview(view)
--- Bento/Views/TableViewHeaderFooterView.swift
+++ Bento/Views/TableViewHeaderFooterView.swift
@@ -3,6 +3,15 @@ import UIKit
 final class TableViewHeaderFooterView: UITableViewHeaderFooterView {
     var containedView: UIView? = nil
 
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        backgroundColor = .clear
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     func install(view: UIView) {
         self.containedView = view
         contentView.addSubview(view)
