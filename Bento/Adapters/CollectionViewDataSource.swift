0f5e51e33778e1f3d3d6de2a8ce7120beec49812
sergdort <sergdort@gmail.com>
2018-08-01 15:47:06 +0100

    add header and footer support for collection view

--- Bento/Adapters/CollectionViewDataSource.swift
+++ Bento/Adapters/CollectionViewDataSource.swift
@@ -3,6 +3,7 @@ import UIKit
 
 final class CollectionViewDataSource<SectionId: Hashable, ItemId: Hashable>
     : NSObject, UICollectionViewDataSource {
+    private let emptyHeaderFooterReuseID = "Bento.CollectionViewDataSource.emptyHeaderFooterReuseID"
     private var sections: [Section<SectionId, ItemId>] = []
     private weak var collectionView: UICollectionView?
 
@@ -62,7 +63,81 @@ final class CollectionViewDataSource<SectionId: Hashable, ItemId: Hashable>
         return cell
     }
 
+    public func collectionView(_ collectionView: UICollectionView,
+                               viewForSupplementaryElementOfKind kind: String,
+                               at indexPath: IndexPath) -> UICollectionReusableView {
+        if kind == UICollectionElementKindSectionHeader {
+            return setupHeader(for: collectionView, at: indexPath)
+        } else {
+            return setupFooter(for: collectionView, at: indexPath)
+        }
+    }
+
     private func node(at indexPath: IndexPath) -> Node<ItemId> {
         return sections[indexPath.section].rows[indexPath.row]
+    }
+
+    private func setupHeader(for collectionView: UICollectionView, at indexPath: IndexPath) -> UICollectionReusableView {
+        guard let component = sections[indexPath.section].header else {
+            collectionView.register(CollectionViewSupplementaryView.self,
+                                    forSupplementaryViewOfKind: UICollectionElementKindSectionHeader,
+                                    withReuseIdentifier: emptyHeaderFooterReuseID)
+            return collectionView.dequeueReusableSupplementaryView(
+                ofKind: UICollectionElementKindSectionHeader,
+                withReuseIdentifier: emptyHeaderFooterReuseID,
+                for: indexPath
+            )
+        }
+        collectionView.register(CollectionViewSupplementaryView.self,
+                                forSupplementaryViewOfKind: UICollectionElementKindSectionHeader,
+                                withReuseIdentifier: component.reuseIdentifier)
+        let cell = collectionView.dequeueReusableSupplementaryView(
+            ofKind: UICollectionElementKindSectionHeader,
+            withReuseIdentifier: component.reuseIdentifier,
+            for: indexPath
+            ) as! CollectionViewSupplementaryView
+
+        let componentView: UIView
+        if let containedView = cell.containedView {
+            componentView = containedView
+        } else {
+            componentView = component.generate()
+            cell.install(view: componentView)
+        }
+        component.render(in: componentView)
+
+        return cell
+    }
+
+    private func setupFooter(for collectionView: UICollectionView, at indexPath: IndexPath) -> UICollectionReusableView {
+        guard let component = sections[indexPath.section].footer else {
+            collectionView.register(CollectionViewSupplementaryView.self,
+                                    forSupplementaryViewOfKind: UICollectionElementKindSectionFooter,
+                                    withReuseIdentifier: emptyHeaderFooterReuseID)
+            return collectionView.dequeueReusableSupplementaryView(
+                ofKind: UICollectionElementKindSectionFooter,
+                withReuseIdentifier: emptyHeaderFooterReuseID,
+                for: indexPath
+            )
+        }
+        collectionView.register(CollectionViewSupplementaryView.self,
+                                forSupplementaryViewOfKind: UICollectionElementKindSectionFooter,
+                                withReuseIdentifier: component.reuseIdentifier)
+        let cell = collectionView.dequeueReusableSupplementaryView(
+            ofKind: UICollectionElementKindSectionFooter,
+            withReuseIdentifier: component.reuseIdentifier,
+            for: indexPath
+        ) as! CollectionViewSupplementaryView
+
+        let componentView: UIView
+        if let containedView = cell.containedView {
+            componentView = containedView
+        } else {
+            componentView = component.generate()
+            cell.install(view: componentView)
+        }
+        component.render(in: componentView)
+
+        return cell
     }
 }
