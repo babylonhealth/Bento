5a482dcd9ca647f23fbb06422b9cc67df12aca6c
NSMyself <joao.pereira@babylonhealth.com>
2019-03-11 17:12:22 +0000

    Swap UIImage for ImageOrLabel property to the Toggle component

--- BentoKit/BentoKit/Components/Toggle.swift
+++ BentoKit/BentoKit/Components/Toggle.swift
@@ -1,4 +1,5 @@
 import Bento
+import ReactiveSwift
 import StyleSheets
 
 extension Component {
@@ -11,7 +12,7 @@ extension Component {
         public init(
             title: String,
             attributedTitle: NSAttributedString? = nil,
-            image: UIImage? = nil,
+            image: Property<BentoKit.ImageOrLabel>? = nil,
             isOn: Bool = false,
             isEnabled: Bool = true,
             isRefreshing: Bool = false,
@@ -33,28 +34,17 @@ extension Component {
                     view.textLabel.text = title
                 }
 
-                view.imageView.image = image
-                view.imageView.isHidden = (image == nil)
+                view.disposable = image?.producer.startWithValues(view.imageOrLabelView.reactive.content.action)
+                view.imageOrLabelView.isHidden = image == nil
                 view.isRefreshing = isRefreshing
             }
 
-            self.heightComputer = { width, inheritedMargins in
-                let verticalMargins = styleSheet.layoutMargins.verticalTotal
-                let imageWidth: CGFloat = image != nil
-                    ? Dimensions.imageViewWidth + Dimensions.horizontalElementsSpacing
-                    : 0.0
-                let textBoundWidth = width
-                    - max(styleSheet.layoutMargins.left, inheritedMargins.left)
-                    - max(styleSheet.layoutMargins.right, inheritedMargins.right)
-                    - Dimensions.toggleWidth
-                    - Dimensions.horizontalElementsSpacing
-                    - imageWidth
-                let textHeight = styleSheet.text.height(of: title, fittingWidth: textBoundWidth)
-                return max(
-                    styleSheet.enforcesMinimumHeight ? Dimensions.minimumCellHeight : 0.0,
-                    textHeight + verticalMargins
-                )
-            }
+            self.heightComputer = Toggle.heightComputer(
+                title: title,
+                attributedTitle: attributedTitle,
+                styleSheet: styleSheet,
+                image: image
+            )
         }
 
         public func estimatedHeight(forWidth width: CGFloat, inheritedMargins: UIEdgeInsets) -> CGFloat {
@@ -67,9 +57,92 @@ extension Component {
     }
 }
 
+private extension Component.Toggle {
+    static func heightComputer(
+        title: String,
+        attributedTitle: NSAttributedString? = nil,
+        styleSheet: StyleSheet,
+        image: Property<ImageOrLabel>?
+        ) -> (CGFloat, UIEdgeInsets) -> CGFloat {
+        return { width, inheritedMargins in
+            guard width > 0 else { return 0 }
+
+            let verticalMargins = styleSheet.layoutMargins.verticalTotal
+            let (imageWidth, imageHeight) = actualSizeWithSpacing(measuring: image, styleSheet: styleSheet)
+            let textBoundWidth = width
+                - max(styleSheet.layoutMargins.left, inheritedMargins.left)
+                - max(styleSheet.layoutMargins.right, inheritedMargins.right)
+                - Dimensions.toggleWidth
+                - Dimensions.horizontalElementsSpacing
+                - imageWidth
+
+            let textHeight: CGFloat
+            // Needed since we've two different overloads (one for an NSAttributedString and another for plain String)
+            if let attributedTitle = attributedTitle {
+                textHeight = styleSheet.text.height(of: attributedTitle, fittingWidth: textBoundWidth)
+            } else {
+                textHeight = styleSheet.text.height(of: title, fittingWidth: textBoundWidth)
+            }
+
+            return max(
+                styleSheet.enforcesMinimumHeight ? Dimensions.minimumCellHeight : 0.0,
+                textHeight + verticalMargins,
+                imageHeight
+            )
+        }
+    }
+
+    static func actualSizeWithSpacing(measuring image: Property<ImageOrLabel>?,
+                                     styleSheet: StyleSheet) -> (CGFloat, CGFloat) {
+        return (
+            Component.Toggle.imageWidthPlusSpacing(measuring: image, styleSheet: styleSheet),
+            Component.Toggle.imageHeight(measuring: image, styleSheet: styleSheet)
+        )
+    }
+
+    static func imageWidthPlusSpacing(measuring image: Property<ImageOrLabel>?,
+                                      styleSheet: StyleSheet) -> CGFloat {
+        let width: CGFloat
+
+        if let size = styleSheet.imageOrLabel.fixedSize {
+            width = size.width
+        } else {
+            switch image?.value {
+            case let .image(image)?:
+                width = image.size.width
+                    + styleSheet.imageOrLabel.layoutMargins.left
+                    + styleSheet.imageOrLabel.layoutMargins.right
+            case let .text(text)?:
+                width = (text as NSString)
+                    .size(withAttributes: [.font: styleSheet.imageOrLabel.label.font])
+                    .width
+                    .rounded(.up)
+            case .none?:
+                width = 0
+            case nil:
+                return 0
+            }
+        }
+
+        return min(width, Dimensions.maxImageViewWidth)
+    }
+
+    static func imageHeight(measuring image: Property<ImageOrLabel>?,
+                                      styleSheet: StyleSheet) -> CGFloat {
+        switch image?.value {
+        case let .image(image)?:
+        return styleSheet.imageOrLabel.fixedSize?.height ?? image.size.height
+        case let .text(text)?:
+            return styleSheet.text.height(of: text, fittingWidth: Dimensions.maxImageViewWidth)
+        default:
+            return 0
+        }
+    }
+}
+
 extension Component.Toggle {
     public final class View: BaseView {
-        fileprivate let imageView = UIImageView().width(32).height(32)
+        fileprivate let imageOrLabelView = ImageOrLabelView()
         fileprivate let toggle = UISwitch().width(50)
         fileprivate let textLabel = UILabel().with {
             $0.numberOfLines = 0
@@ -79,6 +152,11 @@ extension Component.Toggle {
             .with {
                 $0.hidesWhenStopped = true
         }
+
+        fileprivate var disposable: Disposable? {
+            willSet { disposable?.dispose() }
+        }
+
         fileprivate let contentView: UIStackView
         fileprivate var didChangeValue: ((Bool) -> Void)?
         fileprivate var isRefreshing: Bool = false {
@@ -101,7 +179,7 @@ extension Component.Toggle {
                 spacing: 9,
                 distribution: .fill,
                 alignment: .center)(
-                    imageView,
+                    imageOrLabelView,
                     textLabel
             )
 
@@ -128,14 +206,17 @@ extension Component.Toggle {
 extension Component.Toggle {
     public final class StyleSheet: BaseViewStyleSheet<View> {
         public let text: LabelStyleSheet
+        public let imageOrLabel: ImageOrLabelView.StyleSheet
 
-        public init(text: LabelStyleSheet) {
+        public init(text: LabelStyleSheet, imageOrLabel: ImageOrLabelView.StyleSheet = .init()) {
             self.text = text
+            self.imageOrLabel = imageOrLabel
         }
 
         public override func apply(to view: Component.Toggle.View) {
             super.apply(to: view)
             text.apply(to: view.textLabel)
+            imageOrLabel.apply(to: view.imageOrLabelView)
         }
     }
 }
@@ -143,7 +224,7 @@ extension Component.Toggle {
 extension Component.Toggle {
     enum Dimensions {
         static let toggleWidth: CGFloat = 50
-        static let imageViewWidth: CGFloat = 32
+        static let maxImageViewWidth: CGFloat = 100
         static let horizontalElementsSpacing: CGFloat = 9
         static let minimumCellHeight: CGFloat = 44
     }
